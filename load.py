da4ad3b3af0bba120ba7112f0b688d96d028a4d1
gavinbees <gavinabess1@gmail.com>
2026-03-17 10:44:10 -0700

    Update load.py

--- load.py
+++ load.py
@@ -2,7 +2,7 @@ import pandas as pd
 import sqlite3
 
 def load_data(sector_returns, ticker_volatilities, highest_growth, sharpe_ratio, max_drawdowns, ticker_mapping):
-    conn = sqlite3.connect("data/stocks.db")
+    conn = sqlite3.connect("stocks.db")
     sector_returns.to_sql("sector_returns", conn, if_exists="replace", index=False)
     ticker_volatilities.to_sql("ticker_volatilities", conn, if_exists="replace", index=False)
     highest_growth.to_sql("highest_growth", conn, if_exists="replace", index=False)
@@ -18,7 +18,7 @@ Below is a python script to run directly in Power BI if you don't want to host t
 
 import sqlite3
 import pandas as pd
-conn = sqlite3.connect(r'....\data\stocks.db') # Replace with your file path
+conn = sqlite3.connect(r'....\stocks.db') # Replace with your file path
 
 query = "SELECT * FROM sector_returns" 
 sector_returns = pd.read_sql_query(query, conn)
